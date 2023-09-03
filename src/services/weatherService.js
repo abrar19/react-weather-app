87a20e7aa970f9ac2d1394e596723110aeeb154d
Abrar Haque <abm.abrar@gmail.com>
2023-09-04 02:19:19 +0600

    wip

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -7,7 +7,9 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/"
 const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + '/' + infoType);
     url.search = new URLSearchParams({...searchParams, appid: API_KEY})
-
+    
+    console.log(url);
+    
     const fetchData = fetch(url).then((res) => res.json()).then((data)=>data);
 
     return fetchData;
