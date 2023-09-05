7016bd912cc109c189397693dbd909bf2a9d7424
Abrar Haque <abm.abrar@gmail.com>
2023-09-05 13:15:31 +0600

    formatted api data

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,16 +3,55 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
 //Example query from wearher API
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+//Onecall Weather API
+//https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API_KEY}
 
 const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + '/' + infoType);
     url.search = new URLSearchParams({...searchParams, appid: API_KEY})
     
-    console.log(url);
-    
-    const fetchData = fetch(url).then((res) => res.json()).then((data)=>data);
-
+    const fetchData = fetch(url).then((res) => res.json());
     return fetchData;
 }
 
-export default getWeatherData;
+const formatCurrentWeather = (data) => {
+    const {
+        coord: {lat, lon},
+        main: {temp, feels_like, temp_min, temp_max, humidity},
+        name,
+        dt,
+        sys: {country, sunrise, sunset},
+        weather,
+        wind: {speed}
+    } = data;
+
+    const {main:details, icon} = weather[0]
+
+    return {
+        lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed
+    }
+}
+
+const formatForecastWeather = (data) => {
+    let {timezone, daily, hourly} = data;
+    daily = daily.slice(1, 6).map();
+}
+
+const getFormattedWeatherData = async(searchParams) => {
+    const formattedCurrentWeather = await getWeatherData('weather', searchParams).then(formatCurrentWeather);
+
+    const {lat, lon} = formattedCurrentWeather;
+
+    
+
+    const formattedForecastWeather = await getWeatherData('onecall', {
+        lat,
+        lon,
+        exclude: "current, minutely, alerts",
+        units: searchParams.units
+    }).then(formatForecastWeather)
+
+    return formattedCurrentWeather;
+}
+
+export default getFormattedWeatherData;
